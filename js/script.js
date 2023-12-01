86e94df026938af23cbac3d94f8a1c82d2a3df3e
Maurocazzato <maurizzio.125@gmail.com>
2023-12-01 14:43:14 +0100

    stampo su console i memnri del team

--- js/script.js
+++ js/script.js
@@ -30,5 +30,36 @@ const arrayTeamMember = [
         "ruolo": "Chief Editor",
         "img": "angela-caroll-chief-editor.jpg",
 
-    }
+    },
+    {
+        "nome": "Walter Gordon",
+        "ruolo": "Office Manager",
+        "img": "walter-gordon-office-manager.jpg",
+
+    },
+    {
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager",
+        "img": "angela-lopez-social-media-manager.jpg",
+
+    },
+    {
+        "nome": "Scott Estrada",
+        "ruolo": "Developer",
+        "img": "scott-estrada-developer.jpg",
+
+    },
+    {
+        "nome": "Barbara Ramos",
+        "ruolo": "Graphic Designer",
+        "img": "barbara-ramos-graphic-designer.jpg",
+
+    },
 ]
+
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+for(i= 0; i<arrayTeamMember.length; i++){
+    
+    let teamMemberIesimo = arrayTeamMember[i];
+    console.log(teamMemberIesimo);
+}
