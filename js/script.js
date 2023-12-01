ca768ee2b87ff5aabe4e7a30d40c5754c87c0364
Maurocazzato <maurizzio.125@gmail.com>
2023-12-01 14:34:47 +0100

    creo un array con il team

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sotto forma di stringhe
+
+//  Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+const teamMember = {
+    "nome": "",
+    "ruolo": "",
+    "foto": "",
+};
+
+for (let key in teamMember ){
+    console.log(key, teamMember[key]);
+};
+
+// creare un array di oggetti per rappresentare i membri del team.
+const arrayTeamMember = [
+    {
+        "nome": "Wayne Barnett",
+        "ruolo": "Founder & CEO",
+        "img": "wayne-barnett-founder-ceo.jpg",
+
+    },
+    {
+        "nome": "Angela Caroll",
+        "ruolo": "Chief Editor",
+        "img": "angela-caroll-chief-editor.jpg",
+
+    }
+]
